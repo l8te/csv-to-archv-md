90348eb4f8d52bc205d0e109d3c83ef301c1629f
Lee <41347864+l8cantparty@users.noreply.github.com>
2019-03-05 13:33:25 -0500

    Update csv-to-grav-yaml.py
    names .meta.yaml from csv content, formats taxonomy section

--- csv-to-grav-yaml.py
+++ csv-to-grav-yaml.py
@@ -1,15 +1,14 @@
-# modifed from https://github.com/hfionte/csv_to_yaml
-
-# Takes a CSV file and outputs each row as a YAML file.
-# Data in the first row of the CSV is assumed to be the column heading.
+# based on https://github.com/hfionte/csv_to_yaml
 
 # Import the python library for parsing CSV files.
 import csv
+import glob
 
-# Open our data file in read-mode.
-csvfile = open('data.csv', 'r')
+all_csvs = glob.glob('*.csv')
 
-# Save a CSV Reader object.
+# open all CSVs
+csvfile = open(all_csvs[0], 'r')
+
 datareader = csv.reader(csvfile, delimiter=',', quotechar='"')
 
 # Empty array for data headings, which we will fill with the first row from our CSV.
@@ -21,38 +20,39 @@ for row_index, row in enumerate(datareader):
 	# If this is the first row, populate our data_headings variable.
 	if row_index == 0:
 		data_headings = row
+		print(data_headings)
 
-		# Otherwise, create a grav .meta.yaml file from the data in this row
+	# Otherwise, create a grav .meta.yaml file from the data in this row
 	else:
 		# Open a new file with filename based on the correct column from the CSV
 
 		if row_index > 0:
+			# print(row[1])
 			# change this integer to change what column the file name is based on
 			fname = row[1]
-			
-		# change file extension here as needed
+
 		filename = str(fname) + '.jpg.meta.yaml'
 		new_yaml = open(filename, 'w')
 
 		# Empty string that we will fill with yaml formatted text based on data extracted from our CSV.
 		yaml_frontmatter = ""
+		grav_tags = ""
+		taxonomy_header = "taxonomy: " + "\n"
 
-		# Loop through each cell in this row...
-		for cell_index, cell in enumerate(row):
+		for cell_index, cell in enumerate(row[:4]):
 
-			# Compile a line of yaml text from our headings list and the text of the current cell, followed by a linebreak.
-			# Heading text is converted to lowercase. Spaces are converted to underscores and hyphens are removed.
-			# In the cell text, line endings are replaced with commas.
 			cell_heading = data_headings[cell_index].lower().replace(" ", "_").replace("-", "")
 			cell_text = cell_heading + ": " + cell.replace("\n", ", ") + "\n"
 
-			# Add this line of text to the current yaml string.
 			yaml_frontmatter += cell_text
 
-		# Write our yaml string to the new text file and close it.
-		# new_yaml.write(frontmatter_delimiter + "\n" + yaml_frontmatter + frontmatter_delimiter +"\n" + yaml_body)
-		new_yaml.write(yaml_frontmatter)
-		new_yaml.close()
+		for cell_index, cell in enumerate(row[+5:]):
+			# print(cell)
+			
+			cell_heading = data_headings[cell_index].lower().replace(" ", "_").replace("-", "")
+			taxonomy_text = "\t" + cell_heading + ": " + cell.replace("\n", ", ") + "\n"
 
-# We're done! Close the CSV file.
-csvfile.close()
+			grav_tags += taxonomy_text
+
+		new_yaml.write(yaml_frontmatter + taxonomy_header + grav_tags)
+		new_yaml.close()
